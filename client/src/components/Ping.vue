4149b1775c43a26f965e54ef8dbcddf16b265e89
Leestalion <lemarrecthomas@gmail.com>
2022-02-17 23:18:08 +0100

    try another api url

--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
     getMessage() {
-      const path = 'http://127.0.0.1/api/ping';
+      const path = 'http://0.0.0.0:5000/ping';
       axios.get(path)
         .then((res) => {
           this.msg = res.data;
